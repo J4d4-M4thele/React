a74e053abb44d1f9c830a8ef366891f23f2f827d
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-15 12:36:15 +0200

    update

--- chapter09/src/App.js
+++ chapter09/src/App.js
@@ -1,76 +1,30 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import GitHub from './GitHub';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
-import GitHubUser from './GitHubUser';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBxnhEacQcZkJHG7iz0BbW1RWryONe_780",
-  authDomain: "chapter09-90c07.firebaseapp.com",
-  projectId: "chapter09-90c07",
-  storageBucket: "chapter09-90c07.appspot.com",
-  messagingSenderId: "688727815220",
-  appId: "1:688727815220:web:e1d61ee0aad228724d8661",
-  measurementId: "G-XZ4VEVW300"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//import User from './User';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/database';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+//import UserForm from './UserForm';
 
 class App extends Component {
   constructor() {
-    super();
-    console.log(app)
+    super()
+    console.log(firebase);
   }
+
   render() {
     return (
-      <div>
-        <Header />
-      </div>
+        <div>
+        <h1></h1>
+        </div>
     );
   }
 }
+
 export default App;
-
-class Header extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/github">GitHub</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Switch>
-            <Route path="/github/user/:login/:id" component={GitHubUser} />
-            <Route path="/github" component={GitHub} />
-            <Route exact path="/" component={Home} />
-            <Route path="/*" component={NotFound} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    )
-  }
-}
-
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        Home
-      </div>
-    )
-  }
-}
 
 class NotFound extends Component {
   render() {
--- chapter09/src/index.js
+++ chapter09/src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBxnhEacQcZkJHG7iz0BbW1RWryONe_780",
-    authDomain: "chapter09-90c07.firebaseapp.com",
-    projectId: "chapter09-90c07",
-    storageBucket: "chapter09-90c07.appspot.com",
-    messagingSenderId: "688727815220",
-    appId: "1:688727815220:web:e1d61ee0aad228724d8661",
-    measurementId: "G-XZ4VEVW300"
+  apiKey: "AIzaSyBLTgQ5YJOP5EHHiw9giqXzbWMMUhyT0Wg",
+  authDomain: "react2022b.firebaseapp.com",
+  databaseURL: "https://react2022b-default-rtdb.firebaseio.com",
+  projectId: "react2022b",
+  storageBucket: "react2022b.appspot.com",
+  messagingSenderId: "853395810250",
+  appId: "1:853395810250:web:1bf5bcb7021ddf0a3fd133"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -26,3 +27,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
