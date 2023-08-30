4d9f5d362b925d174f505afb99f47498984193eb
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 15:40:48 +0200

    update

--- Extra Exercises/react_lists/src/App.js
+++ Extra Exercises/react_lists/src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Garage from './Garage';
 
 function App() {
-  //***with JSX***
-  // const myElement = <h1>I love JSX!</h1>
-  const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(myElement);
-
-
-
-  //***without JSX***
-  //   const myElement = React.createElement('h1', {}, 'I do not use JSX!');
-
-  //   const root = ReactDOM.createRoot(document.getElementById('root'));
-  //   root.render(myElement);
+  return <Garage/>
 }
 
 export default App;
