66b506d21d7dd999cc8a6689f0e6c066d7d042fe
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 15:28:57 +0200

    update

--- Extra Exercises/react_forms/src/MyForm.js
+++ Extra Exercises/react_forms/src/MyForm.js
@@ -24,42 +24,59 @@ function MyForm() {
 */
 
 //MULTIPLE INPUT FIELDS
+// function MyForm() {
+//     const [inputs, setInputs] = useState({});
+//     const [textarea, setTextarea] = useState(
+//         "The content of a textarea goes in the value attribute"
+//     );
+
+//     const handleChange = (event) => {
+//         const name = event.target.name;
+//         const value = event.target.value;
+//         setInputs(values => ({ ...values, [name]: value }))
+//         setTextarea(event.target.value);
+//     }
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+//         alert(inputs);
+//     }
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <label>Enter your name:
+//                 <input
+//                     type="text"
+//                     name="username"
+//                     value={inputs.username || ""}
+//                     onChange={handleChange}
+//                 />
+//             </label>
+//             <label>Enter your age:
+//                 <input
+//                     type="number"
+//                     name="age"
+//                     value={inputs.age || ""}
+//                     onChange={handleChange}
+//                 />
+//             </label>
+//             <input type="submit" />
+//         </form>
+//     )
+// }
+
+//TEXT AREA(allows chane to text and size)
 function MyForm() {
-    const [inputs, setInputs] = useState({});
     const [textarea, setTextarea] = useState(
         "The content of a textarea goes in the value attribute"
     );
 
     const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
-        setTextarea(event.target.value);
+        setTextarea(event.target.value)
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(inputs);
-    }
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Enter your name:
-                <input
-                    type="text"
-                    name="username"
-                    value={inputs.username || ""}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>Enter your age:
-                <input
-                    type="number"
-                    name="age"
-                    value={inputs.age || ""}
-                    onChange={handleChange}
-                />
-            </label>
-            <input type="submit" />
+        <form>
+            <textarea value={textarea} onChange={handleChange} />
         </form>
     )
 }
