6b9c4b3a568d94a49994a5edf34eb39b0d31a193
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 16:09:20 +0200

    update

--- Extra Exercises/sass_styling/src/App.js
+++ Extra Exercises/sass_styling/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
+import './my-sass.scss';
+import Header from './Header';
 
 function App() {
-  //***with JSX***
-  // const myElement = <h1>I love JSX!</h1>
-  const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(myElement);
-
-
-
-  //***without JSX***
-  //   const myElement = React.createElement('h1', {}, 'I do not use JSX!');
-
-  //   const root = ReactDOM.createRoot(document.getElementById('root'));
-  //   root.render(myElement);
+  return <Header />
 }
 
 export default App;
