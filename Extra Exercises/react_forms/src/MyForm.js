22b8948fa6ddcc23b4ed0b74ac7bea187482ffe0
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 15:16:56 +0200

    update

--- Extra Exercises/react_forms/src/MyForm.js
+++ Extra Exercises/react_forms/src/MyForm.js
@@ -2,9 +2,13 @@ import { useState } from 'react';
 
 function MyForm() {
   const [name, setName] = useState("");
-
+  //event
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>Enter your name:
         <input
           type="text" 
@@ -12,6 +16,7 @@ function MyForm() {
           onChange={(e) => setName(e.target.value)}
         />
       </label>
+      <input type="submit" />
     </form>
   )
 }
