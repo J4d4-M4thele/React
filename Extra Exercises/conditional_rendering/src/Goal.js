3e33032c6e9314cb5b938fb2ae8ac862e9edddca
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 14:38:33 +0200

    update

--- Extra Exercises/conditional_rendering/src/Goal.js
+++ Extra Exercises/conditional_rendering/src/Goal.js
@@ -1,5 +1,11 @@
 function Goal(props) {
     const isGoal = props.isGoal;
+    function MissedGoal() {
+        return <h1>MISSED!</h1>;
+      }
+      function MadeGoal() {
+        return <h1>Goal!</h1>;
+      }
     if (isGoal) {
         return <MadeGoal />;
     }
