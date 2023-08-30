d11bc1f15a05eb1169ec118274ef42af85eaf6b7
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 15:26:15 +0200

    update

--- Extra Exercises/react_forms/src/MyForm.js
+++ Extra Exercises/react_forms/src/MyForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+/*ONE INPUT FIELD
 function MyForm() {
   const [name, setName] = useState("");
   //event
@@ -19,6 +20,48 @@ function MyForm() {
       <input type="submit" />
     </form>
   )
+}
+*/
+
+//MULTIPLE INPUT FIELDS
+function MyForm() {
+    const [inputs, setInputs] = useState({});
+    const [textarea, setTextarea] = useState(
+        "The content of a textarea goes in the value attribute"
+    );
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+        setTextarea(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Enter your name:
+                <input
+                    type="text"
+                    name="username"
+                    value={inputs.username || ""}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Enter your age:
+                <input
+                    type="number"
+                    name="age"
+                    value={inputs.age || ""}
+                    onChange={handleChange}
+                />
+            </label>
+            <input type="submit" />
+        </form>
+    )
 }
 
 export default MyForm;
