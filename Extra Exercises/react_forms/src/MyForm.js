bd9c10a50e9a08c3db022a3d7831861a57478058
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 15:33:19 +0200

    update

--- Extra Exercises/react_forms/src/MyForm.js
+++ Extra Exercises/react_forms/src/MyForm.js
@@ -65,18 +65,36 @@ function MyForm() {
 // }
 
 //TEXT AREA(allows chane to text and size)
+// function MyForm() {
+//     const [textarea, setTextarea] = useState(
+//         "The content of a textarea goes in the value attribute"
+//     );
+
+//     const handleChange = (event) => {
+//         setTextarea(event.target.value)
+//     }
+
+//     return (
+//         <form>
+//             <textarea value={textarea} onChange={handleChange} />
+//         </form>
+//     )
+// }
+
 function MyForm() {
-    const [textarea, setTextarea] = useState(
-        "The content of a textarea goes in the value attribute"
-    );
+    const [myCar, setMyCar] = useState("Volvo");
 
     const handleChange = (event) => {
-        setTextarea(event.target.value)
+        setMyCar(event.target.value)
     }
 
     return (
         <form>
-            <textarea value={textarea} onChange={handleChange} />
+            <select value={myCar} onChange={handleChange}>
+                <option value="Ford">Ford</option>
+                <option value="Volvo">Volvo</option>
+                <option value="Fiat">Fiat</option>
+            </select>
         </form>
     )
 }
