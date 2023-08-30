2e625ac93ded36d48c1ad18577ab9b6aff762945
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 11:24:54 +0200

    update

--- Extra Exercises/react_props/src/App.js
+++ Extra Exercises/react_props/src/App.js
@@ -1,11 +1,23 @@
+import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+//props stands for properties
 function Car(props) {
   return <h2>I am a { props.brand }!</h2>;
 }
+//pass value into car component (function)
+//const myElement = <Car brand="Ford" />;
 
-const myElement = <Car brand="Ford" />;
+function Garage() {
+  return (
+    <>
+    <h1>Who lives in my garage?</h1>
+    <Car brand="Ford" />
+    </>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement);
+root.render(<Garage />);
+
+export default App;
