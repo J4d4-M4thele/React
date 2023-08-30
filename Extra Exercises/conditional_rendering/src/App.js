b74f0a634a501d3338807557079b33b261007ab4
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-30 14:35:59 +0200

    update

--- Extra Exercises/conditional_rendering/src/App.js
+++ Extra Exercises/conditional_rendering/src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import Goal from './Goal';
 
 function App() {
-  //***with JSX***
-  // const myElement = <h1>I love JSX!</h1>
-  const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(myElement);
-
-
-
-  //***without JSX***
-  //   const myElement = React.createElement('h1', {}, 'I do not use JSX!');
-
-  //   const root = ReactDOM.createRoot(document.getElementById('root'));
-  //   root.render(myElement);
+  return <Goal />
 }
 
 export default App;
