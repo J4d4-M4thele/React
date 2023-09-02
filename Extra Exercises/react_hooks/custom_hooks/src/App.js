8616f50afd467ec6a1744efe8793ea0b4bf2fa92
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-09-02 16:23:09 +0200

    update

--- Extra Exercises/react_hooks/custom_hooks/src/App.js
+++ Extra Exercises/react_hooks/custom_hooks/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
-import React from 'react';
-import FavoriteColor from './FavoriteColor';
+import { useState, useEffect } from "react";
 
-function App() {
-  return <FavoriteColor/>
-}
+const Home = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+ }, []);
+
+  return (
+    <>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </>
+  );
+};
 
 export default App;
